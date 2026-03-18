b45ea0bfc94cf3338cfd131c5d230a5dcff79f0e
GitHub Action <action@github.com>
2026-03-18 04:24:13 +0000

    Sync to proto commit
    runetale/proto@8cacad104e43322cdf7d416494d49e7b5bb41576

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-pub mod runetale {
-    pub mod v1 {
-        include!("runetale.runetale.v1.rs");
-    }
+pub mod protos {
+    include!("protos/protos.rs");
+    include!("protos/protos.tonic.rs");
+}
+
+pub mod logserver {
+    include!("logserver/logserver.rs");
+    include!("logserver/logserver.tonic.rs");
 }
